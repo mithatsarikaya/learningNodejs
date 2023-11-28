3891abd690030b8c979b3380c9b0319d92f6db73
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-11-28 15:35:18 +0300

    getting title of the .mdx file from frontmatter of it. creating html template to screenshot for ogImage

--- createPngByFile/dist/index.js
+++ createPngByFile/dist/index.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log("Hello world!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+//__dirname: where this file is at.
+const pagesDir = path_1.default.resolve(__dirname, "../pages"); //returns path
+const findMdxFiles = (dir) => {
+    const files = fs_1.default.readdirSync(dir);
+    const mdxFiles = [];
+    for (const file of files) {
+        const filePath = path_1.default.join(dir, file);
+        const stats = fs_1.default.statSync(filePath);
+        if (stats.isDirectory()) {
+            mdxFiles.push(...findMdxFiles(filePath));
+        }
+        else if (file.endsWith(".mdx")) {
+            mdxFiles.push(filePath);
+        }
+    }
+    return mdxFiles;
+};
+const mdxFiles = findMdxFiles(pagesDir);
+let firstOnesPath = mdxFiles[1];
+const mdxContent = fs_1.default.readFileSync(firstOnesPath, "utf8");
+let titleOfTheMdx = (_b = (_a = mdxContent
+    .split("---", 2)
+    .find((elem) => elem.includes("title"))) === null || _a === void 0 ? void 0 : _a.split("\n").find((elem) => elem.includes("title"))) === null || _b === void 0 ? void 0 : _b.split('"')[1];
+if (!titleOfTheMdx)
+    throw new Error(`there is no title at ${firstOnesPath}`);
+console.log(titleOfTheMdx);
+// let frontmatterContent =
--- createPngByFile/src/index.ts
+++ createPngByFile/src/index.ts
@@ -1,1 +1,46 @@
-console.log("Hello world!");
+import fs from "fs";
+import path from "path";
+import puppeteer from "puppeteer";
+import { fileURLToPath } from "node:url";
+
+//__dirname: where this file is at.
+const pagesDir = path.resolve(__dirname, "../pages"); //returns path
+
+const findMdxFiles = (dir: string): string[] => {
+  const files = fs.readdirSync(dir);
+  const mdxFiles: string[] = [];
+
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      mdxFiles.push(...findMdxFiles(filePath));
+    } else if (file.endsWith(".mdx")) {
+      mdxFiles.push(filePath);
+    }
+  }
+
+  return mdxFiles;
+};
+
+let propertyOfTheFrontmatterForOgImage = "title";
+
+const mdxFiles = findMdxFiles(pagesDir);
+let firstOnesPath = mdxFiles[1];
+const mdxContent = fs.readFileSync(firstOnesPath, "utf8");
+let titleOfTheMdx = mdxContent
+  .split("---", 2)
+  .find((elem) => elem.includes(propertyOfTheFrontmatterForOgImage))
+  ?.split("\n")
+  .find((elem) => elem.includes(propertyOfTheFrontmatterForOgImage))
+  ?.split('"')[1];
+
+if (!titleOfTheMdx)
+  throw new Error(
+    `there is no ${propertyOfTheFrontmatterForOgImage} at ${firstOnesPath}`
+  );
+
+console.log(titleOfTheMdx);
+
+// let frontmatterContent =
